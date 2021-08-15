364ec76548ad3fe6af7ab077f7574ff4eb6cf02b
Matt <me@curoviyx.ru>
2021-08-15 22:49:37 +0300

    Basic information

--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
 #include "mainwindow.h"
 
 #include <QtGui/QApplication>
+#include <QTextCodec>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+
+    QApplication::setApplicationVersion("0.9.0");
+    QApplication::setApplicationName("Kutegram");
+    QApplication::setOrganizationName("curoviyxru");
+    QApplication::setOrganizationDomain("curoviyx.ru");
+
+    QTextCodec *codec = QTextCodec::codecForName("UTF-8");
+    QTextCodec::setCodecForTr(codec);
+    QTextCodec::setCodecForCStrings(codec);
+    QTextCodec::setCodecForLocale(codec);
 
     MainWindow mainWindow;
     mainWindow.setOrientation(MainWindow::ScreenOrientationAuto);
