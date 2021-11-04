23cbeccc7090928b221b4c95ba523138f220c6aa
curoviyxru <me@curoviyx.ru>
2021-11-04 23:40:08 +0300

    README, buildscripts

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Kutegram
+## How to build
+### Symbian^1 (also Symbian^3)
+* Clone this repository with submodules.
+```
+git clone --recursive https://github.com/kutegram/client.git
+cd client
+```
+* Go to `library` folder, rename `apivalues.default.h` file to `apivalues.h`, and fill needed fields.
+* Run build script.
+```
+buildSymbian.bat
+```
---
+++ buildSymbian.bat
@@ -0,0 +1,10 @@
+git clone --branch Symbian1Qt473 https://github.com/kutegram/compilers.git Symbian1Qt473
+call Symbian1Qt473\patch.qmake.paths.bat
+cd ..\..\..
+cd Symbian1Qt473
+call bin\qtenvS1.bat
+cd ..
+lrelease client.pro
+qmake.exe client.pro -r -spec symbian-abld "CONFIG+=release" -after "OBJECTS_DIR=obj" "MOC_DIR=moc" "UI_DIR=ui" "RCC_DIR=rcc"
+make.exe release-gcce -w in .
+make.exe clean -w in .
