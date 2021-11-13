e19af77498ee464ff3a85b91aa06f9572ba757e9
Konstantin Baranovskiy <baranovskiykonstantin@gmail.com>
2021-11-13 19:44:48 +0200

    Ukrainian translation

---
+++ translations/kutegram_uk.ts
@@ -0,0 +1,158 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="uk_UA">
+<context>
+    <name>HistoryWindow</name>
+    <message>
+        <location filename="../historywindow.ui" line="17"/>
+        <source>Kutegram</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../historywindow.ui" line="55"/>
+        <source>...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../historyitemdelegate.cpp" line="81"/>
+        <source>Loading...</source>
+        <translation>Завантаження...</translation>
+    </message>
+    <message>
+        <location filename="../historywindow.cpp" line="21"/>
+        <source>Type a message...</source>
+        <translation>Написати повідомлення...</translation>
+    </message>
+    <message>
+        <location filename="../historywindow.cpp" line="30"/>
+        <source>Options</source>
+        <translation>Опції</translation>
+    </message>
+    <message>
+        <location filename="../historywindow.cpp" line="44"/>
+        <source>Back</source>
+        <translation>Назад</translation>
+    </message>
+</context>
+<context>
+    <name>LoginTypeDialog</name>
+    <message>
+        <location filename="../logintypedialog.ui" line="17"/>
+        <source>Log in</source>
+        <translation>Авторизація</translation>
+    </message>
+    <message>
+        <location filename="../logintypedialog.ui" line="30"/>
+        <source>Phone number</source>
+        <translation>Номер телефону</translation>
+    </message>
+    <message>
+        <location filename="../logintypedialog.ui" line="40"/>
+        <source>QR-code</source>
+        <translation>QR-код</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.ui" line="17"/>
+        <location filename="../mainwindow.cpp" line="142"/>
+        <source>Kutegram</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="69"/>
+        <source>Actions</source>
+        <translation>Дії</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="81"/>
+        <source>Help</source>
+        <translation>Допомога</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="91"/>
+        <source>Log in</source>
+        <translation>Авторизуватись</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="96"/>
+        <location filename="../mainwindow.cpp" line="54"/>
+        <source>Exit</source>
+        <translation>Вихід</translation>
+    </message>
+    <message>
+        <source>Quit</source>
+        <translation type="obsolete">Вийти</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="104"/>
+        <source>Log out</source>
+        <translation>Вийти з акауту</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="109"/>
+        <source>About Kutegram</source>
+        <translation>Про Kutegram</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="117"/>
+        <source>About Qt</source>
+        <translation>Про Qt</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="39"/>
+        <source>Options</source>
+        <translation>Опції</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="143"/>
+        <source>Kutegram by curoviyxru
+An unofficial Qt-based client for Telegram messenger.
+Project&apos;s website: http://kg.curoviyx.ru
+Telegram channel: https://t.me/kutegram
+Telegram chat: https://t.me/kutegramchat</source>
+        <translation>Kutegram, зробленний curoviyxru
+Неофіційний клієнт Telegram, оснований на Qt.
+Сайт проекту: http://kg.curoviyx.ru
+Канал Telegram: https://t.me/kutegram
+Чат Telegram: https://t.me/kutegramchat</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="156"/>
+        <location filename="../mainwindow.cpp" line="166"/>
+        <location filename="../mainwindow.cpp" line="176"/>
+        <location filename="../mainwindow.cpp" line="192"/>
+        <source>Error</source>
+        <translation>Помилка</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="157"/>
+        <source>Got socket error: %1</source>
+        <translation>Помилка сокету: %1</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="167"/>
+        <source>Got MT error: %1</source>
+        <translation>Помилка МТ: %1</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="177"/>
+        <source>Got DH error: %1</source>
+        <translation>Помилка DH: %1</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="193"/>
+        <source>Got RPC error: %1, %2, %3</source>
+        <translation>Помилка RPC: %1, %2, %3</translation>
+    </message>
+</context>
+<context>
+    <name>QRLoginDialog</name>
+    <message>
+        <location filename="../qrlogindialog.ui" line="17"/>
+        <source>QR-code login</source>
+        <translation>Авторизація за QR-кодом</translation>
+    </message>
+</context>
+</TS>
