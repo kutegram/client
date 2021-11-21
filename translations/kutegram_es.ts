1730da5063e6bb8dfd3ba08ea18581628ff0d2a5
huhungahuru <72929651+huhungahuru@users.noreply.github.com>
2021-11-21 19:29:22 +0000

    American Spanish translation.

---
+++ translations/kutegram_es.ts
@@ -0,0 +1,159 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="es_419">
+<context>
+    <name>HistoryWindow</name>
+    <message>
+        <location filename="../historywindow.ui" line="17"/>
+        <source>Kutegram</source>
+        <translation>Kutegram</translation>
+    </message>
+    <message>
+        <location filename="../historywindow.ui" line="55"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <location filename="../historyitemdelegate.cpp" line="81"/>
+        <source>Loading...</source>
+        <translation>Actualizando...</translation>
+    </message>
+    <message>
+        <location filename="../historywindow.cpp" line="21"/>
+        <source>Type a message...</source>
+        <translation>Nuevo mensaje...</translation>
+    </message>
+    <message>
+        <location filename="../historywindow.cpp" line="30"/>
+        <source>Options</source>
+        <translation>Config.</translation>
+    </message>
+    <message>
+        <location filename="../historywindow.cpp" line="44"/>
+        <source>Back</source>
+        <translation>Atrás</translation>
+    </message>
+</context>
+<context>
+    <name>LoginTypeDialog</name>
+    <message>
+        <location filename="../logintypedialog.ui" line="17"/>
+        <source>Log in</source>
+        <translation>Iniciar sesión</translation>
+    </message>
+    <message>
+        <location filename="../logintypedialog.ui" line="30"/>
+        <source>Phone number</source>
+        <translation>Núm. teléfono</translation>
+    </message>
+    <message>
+        <location filename="../logintypedialog.ui" line="40"/>
+        <source>QR-code</source>
+        <translation>Código QR</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.ui" line="17"/>
+        <location filename="../mainwindow.cpp" line="142"/>
+        <source>Kutegram</source>
+        <translation>Kutegram</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="69"/>
+        <source>Actions</source>
+        <translation>Acceder</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="81"/>
+        <source>Help</source>
+        <translation>Ayuda</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="91"/>
+        <source>Log in</source>
+        <translation>Iniciar sesión</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="96"/>
+        <source>Quit</source>
+        <translation>Cerrar</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="104"/>
+        <source>Log out</source>
+        <translation>Finalizar sesión</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="109"/>
+        <source>About Kutegram</source>
+        <translation>Acerca de Kutegram</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="117"/>
+        <source>About Qt</source>
+        <translation>Acerca de Qt</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="39"/>
+        <source>Options</source>
+        <translation>Opciones</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="54"/>
+        <source>Exit</source>
+        <translation>Salir</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="143"/>
+        <source>Kutegram by curoviyxru
+An unofficial Qt-based client for Telegram messenger.
+Project&apos;s website: http://kg.curoviyx.ru
+Telegram channel: https://t.me/kutegram
+Telegram chat: https://t.me/kutegramchat</source>
+        <translation>Kutegram hecho por curoviyxru.
+Cliente no oficial de Telegram basado en Qt.
+Web: http://kg.curoviyx.ru
+Canal: https://t.me/s/kutegram
+Chat: https://t.me/kutegramchat</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="156"/>
+        <location filename="../mainwindow.cpp" line="166"/>
+        <location filename="../mainwindow.cpp" line="176"/>
+        <location filename="../mainwindow.cpp" line="192"/>
+        <source>Error</source>
+        <translation>Error</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="157"/>
+        <source>Got socket error: %1</source>
+        <translation>Error de conexión: %1</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="167"/>
+        <source>Got MT error: %1</source>
+        <translation>Error MT: %1</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="177"/>
+        <source>Got DH error: %1</source>
+        <translation>Error DH: %1</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="193"/>
+        <source>Got RPC error: %1, %2, %3</source>
+        <translatorcomment>Llamada a procedimiento remoto.</translatorcomment>
+        <translation>Error RPC: %1, %2, %3</translation>
+    </message>
+</context>
+<context>
+    <name>QRLoginDialog</name>
+    <message>
+        <location filename="../qrlogindialog.ui" line="17"/>
+        <source>QR-code login</source>
+        <translation>Iniciar sesión por Código QR</translation>
+    </message>
+</context>
+</TS>
