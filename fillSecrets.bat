1eda8118c2c0f1b897d212e886856d8699cbdac6
curoviyxru <me@curoviyx.ru>
2021-11-27 22:30:36 +0300

    GitHub Action build fix

--- fillSecrets.bat
+++ fillSecrets.bat
@@ -3,5 +3,5 @@ echo #define DC_IP "149.154.167.50" >> apivalues.h
 echo #define DC_PORT 443 >> apivalues.h
 echo #define DC_NUMBER 2 >> apivalues.h
 echo #define APP_ID %TG_API_ID% >> apivalues.h
-echo #define APP_HASH "%$TG_API_HASH%" >> apivalues.h
+echo #define APP_HASH "%TG_API_HASH%" >> apivalues.h
 cd ..
