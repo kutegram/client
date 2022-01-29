de8bab86e799fbdf572b3e4941c907cca8aacd16
Matt <me@curoviyx.ru>
2022-01-30 00:08:23 +0300

    Update Telegram CI #2

--- telegram-ci.py
+++ telegram-ci.py
@@ -5,10 +5,10 @@ url = 'https://api.telegram.org/bot' + str(os.environ['BOT_TOKEN']) + '/sendDocu
 
 f = {
     'document': (str(os.environ['UPLOAD_FILE']), open(str(os.environ['UPLOAD_FILE']), 'rb')),
-    'caption': (None, """*%s* pushed commit:
+    'caption': (None, """*%s(https://github.com/$s)* pushed commit:
                          [%s](https://github.com/$s/commit/$s): `$s`
                          
-                         #kutegram #symbian""" % (str(os.environ['COMMIT_AUTHOR']), str(os.environ['COMMIT_SHA']), str(os.environ['COMMIT_REPO']), str(os.environ['COMMIT_SHA']), str(os.environ['COMMIT_MESSAGE']))),
+                         #kutegram #symbian""" % (str(os.environ['COMMIT_AUTHOR']), str(os.environ['COMMIT_USERNAME']), str(os.environ['COMMIT_SHA'][:7]), str(os.environ['COMMIT_REPO']), str(os.environ['COMMIT_SHA']), str(os.environ['COMMIT_MESSAGE']))),
     'chat_id': (None, str(os.environ['BOT_CHANNEL'])),
     'parse_mode': (None, 'MarkdownV2'),
     'disable_notification': (None, 'false'),
