6a2782a7842f6d88d4ca0494f3a64a82efbc694f
Matt <me@curoviyx.ru>
2022-01-30 00:48:49 +0300

    Update Telegram CI #5

--- telegram-ci.py
+++ telegram-ci.py
@@ -5,9 +5,9 @@ url = 'https://api.telegram.org/bot' + str(os.environ['BOT_TOKEN']) + '/sendDocu
 
 f = {
     'document': (str(os.environ['UPLOAD_FILE']), open(str(os.environ['UPLOAD_FILE']), 'rb')),
-    'caption': (None, '*{}(https://github.com/{})* pushed commit:\n[{}](https://github.com/{}/commit/{}): `{}`\n\n#kutegram #symbian'.format(str(os.environ['COMMIT_AUTHOR']), str(os.environ['COMMIT_USERNAME']), str(os.environ['COMMIT_SHA'][:7]), str(os.environ['COMMIT_REPO']), str(os.environ['COMMIT_SHA']), str(os.environ['COMMIT_MESSAGE']))),
+    'caption': (None, '*[{}](https://github.com/{})* pushed commit:\n[{}](https://github.com/{}/commit/{}): `{}`\n\n\#kutegram \#symbian'.format(str(os.environ['COMMIT_AUTHOR']), str(os.environ['COMMIT_USERNAME']), str(os.environ['COMMIT_SHA'][:7]), str(os.environ['COMMIT_REPO']), str(os.environ['COMMIT_SHA']), str(os.environ['COMMIT_MESSAGE']))),
     'chat_id': (None, str(os.environ['BOT_CHANNEL'])),
     'parse_mode': (None, 'MarkdownV2')
 }
 r = requests.post(url, files=f)
-print(r.content)
+print(r)
