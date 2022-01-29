0c9752b78fa9a3d9a302f3253838586fec0146d3
Matt <me@curoviyx.ru>
2022-01-30 00:37:16 +0300

    Update Telegram CI #4

--- telegram-ci.py
+++ telegram-ci.py
@@ -7,9 +7,7 @@ f = {
     'document': (str(os.environ['UPLOAD_FILE']), open(str(os.environ['UPLOAD_FILE']), 'rb')),
     'caption': (None, '*{}(https://github.com/{})* pushed commit:\n[{}](https://github.com/{}/commit/{}): `{}`\n\n#kutegram #symbian'.format(str(os.environ['COMMIT_AUTHOR']), str(os.environ['COMMIT_USERNAME']), str(os.environ['COMMIT_SHA'][:7]), str(os.environ['COMMIT_REPO']), str(os.environ['COMMIT_SHA']), str(os.environ['COMMIT_MESSAGE']))),
     'chat_id': (None, str(os.environ['BOT_CHANNEL'])),
-    'parse_mode': (None, 'MarkdownV2'),
-    'disable_notification': (None, 'false'),
-    'protect_content': (None, 'false')
+    'parse_mode': (None, 'MarkdownV2')
 }
 r = requests.post(url, files=f)
-print(r)
+print(r.content)
