203f25198e171e893d2392254b7dd9aa28d6b4c1
Matt <me@curoviyx.ru>
2022-03-29 01:17:07 +0300

    Messages entities support

--- src/historyitemdelegate.cpp
+++ src/historyitemdelegate.cpp
@@ -21,24 +21,27 @@ HistoryItemDelegate::HistoryItemDelegate(QObject *parent) :
 QString parseHTML(TObject message)
 {
     if (ID(message) != Message) {
+        qDebug() << "[parseHTML] Invalid object id: object is not a message.";
         return QString();
     }
 
-    QString html = message["message"].toString();
+    QStringList items;
+    QList<char> count;
+
+    items << message["message"].toString() + ' ';
+    count << true;
     TVector entities = message["entities"].toList();
 
     for (qint32 i = 0; i < entities.size(); ++i) {
         TObject entity = entities[i].toMap();
         qint32 offset = entity["offset"].toInt();
-        qint32 width = entity["width"].toInt();
+        qint32 length = entity["length"].toInt();
 
-        qDebug() << entity;
-
-        //messageEntityMentionName -> user_id:long
-        //inputMessageEntityMentionName -> user_id:InputUser
+        QString sTag;
+        QString eTag;
 
         switch (ID(entity)) {
-        case MessageEntityUnknown:
+        case MessageEntityUnknown: //just ignore that.
             break;
 
         case MessageEntityMention:
@@ -53,12 +56,21 @@ QString parseHTML(TObject message)
             break;
 
         case MessageEntityBold:
+            sTag = "<b>";
+            eTag = "</b>";
             break;
         case MessageEntityItalic:
+            sTag = "<i>";
+            eTag = "</i>";
             break;
         case MessageEntityCode:
+            sTag = "<code>";
+            eTag = "</code>";
             break;
         case MessageEntityPre:
+            //sTag = "<pre language=\"" + entity["language"].toString() + "\">";
+            sTag = "<pre>";
+            eTag = "</pre>";
             break;
 
         case MessageEntityTextUrl:
@@ -73,8 +85,12 @@ QString parseHTML(TObject message)
             break;
 
         case MessageEntityUnderline:
+            sTag = "<u>";
+            eTag = "</u>";
             break;
         case MessageEntityStrike:
+            sTag = "<s>";
+            eTag = "</s>";
             break;
         case MessageEntityBlockquote:
             break;
@@ -85,9 +101,53 @@ QString parseHTML(TObject message)
         case MessageEntitySpoiler:
             break;
         }
+
+        qint32 posStart = 0;
+        qint32 jidStart = -1;
+        qint32 posEnd = 0;
+        qint32 jidEnd = -1;
+        for (qint32 j = 0; j < items.size(); ++j) {
+            if (!count[j]) continue;
+
+            posStart += items[j].length();
+            posEnd += items[j].length();
+            if (posStart > offset && jidStart == -1) {
+                posStart = items[j].length() - posStart + offset;
+                jidStart = j;
+            }
+            if (posEnd > offset + length && jidEnd == -1) {
+                posEnd = items[j].length() - posEnd + offset + length;
+                jidEnd = j;
+            }
+            if (jidStart != -1 && jidEnd != -1) break;
+        }
+
+        //qDebug() << "[DEBUG]" << message["message"].toString().mid(offset, length);
+
+        if (jidStart == -1 || jidEnd == -1) continue;
+
+        QString sItem = items[jidStart];
+        items[jidStart] = sItem.mid(0, posStart);
+        items.insert(jidStart + 1, sTag);
+        items.insert(jidStart + 2, sItem.mid(posStart));
+        count.insert(jidStart + 1, false);
+        count.insert(jidStart + 2, true);
+
+        if (jidEnd == jidStart) posEnd -= posStart;
+        jidEnd += 2;
+
+        QString eItem = items[jidEnd];
+        items[jidEnd] = eItem.mid(0, posEnd);
+        items.insert(jidEnd + 1, eTag);
+        items.insert(jidEnd + 2, eItem.mid(posEnd));
+        count.insert(jidEnd + 1, false);
+        count.insert(jidEnd + 2, true);
     }
 
-    return html;
+    if (items.last() == " ") items.removeLast();
+    else items.last().chop(1);
+
+    return items.join("").replace('\n', "<br>");
 }
 
 QSize paintContent(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index)
@@ -109,11 +169,15 @@ QSize paintContent(QPainter *painter, const QStyleOptionViewItem &option, const 
     doc.setDefaultFont(font);
 
     if (!painter)
-        return QSize(doc.size().toSize());
+        return QSize(w, doc.size().toSize().height());
 
     QApplication::style()->drawControl(QStyle::CE_ItemViewItem, &option, painter);
 
-    doc.drawContents(painter);
+    QPixmap htmlPixmap(doc.size().toSize());
+    htmlPixmap.fill(Qt::transparent);
+    QPainter htmlPainter(&htmlPixmap);
+    doc.drawContents(&htmlPainter);
+    painter->drawPixmap(x, y, htmlPixmap);
 
     return QSize();
 }
