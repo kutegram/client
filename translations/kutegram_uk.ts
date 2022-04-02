3082a784a32aae92b7e4e5a068325dabd9b58899
Jaroslaw <92401420+AlesAlte@users.noreply.github.com>
2022-04-03 00:16:10 +0300

    Ukrainian translation

--- translations/kutegram_uk.ts
+++ translations/kutegram_uk.ts
@@ -4,152 +4,136 @@
 <context>
     <name>HistoryWindow</name>
     <message>
-        <location filename="../historywindow.ui" line="17"/>
+        <location filename="../../src/historywindow.ui" line="17"/>
         <source>Kutegram</source>
-        <translation></translation>
+        <translation>Kutegram</translation>
     </message>
     <message>
-        <location filename="../historywindow.ui" line="55"/>
+        <location filename="../../src/historywindow.ui" line="70"/>
         <source>...</source>
-        <translation></translation>
+        <translation>...</translation>
     </message>
     <message>
-        <location filename="../historyitemdelegate.cpp" line="81"/>
-        <source>Loading...</source>
-        <translation>Завантаження...</translation>
-    </message>
-    <message>
-        <location filename="../historywindow.cpp" line="21"/>
+        <location filename="../../src/historywindow.cpp" line="38"/>
         <source>Type a message...</source>
-        <translation>Написати повідомлення...</translation>
+        <translation>Введіть повідомлення...</translation>
     </message>
     <message>
-        <location filename="../historywindow.cpp" line="30"/>
+        <location filename="../../src/historywindow.cpp" line="47"/>
         <source>Options</source>
         <translation>Опції</translation>
     </message>
     <message>
-        <location filename="../historywindow.cpp" line="44"/>
+        <location filename="../../src/historywindow.cpp" line="61"/>
         <source>Back</source>
         <translation>Назад</translation>
     </message>
 </context>
 <context>
-    <name>LoginTypeDialog</name>
-    <message>
-        <source>Log in</source>
-        <translation type="obsolete">Авторизація</translation>
-    </message>
-    <message>
-        <source>Phone number</source>
-        <translation type="obsolete">Номер телефону</translation>
-    </message>
-    <message>
-        <source>QR-code</source>
-        <translation type="obsolete">QR-код</translation>
-    </message>
-</context>
-<context>
     <name>MainWindow</name>
     <message>
-        <location filename="../mainwindow.ui" line="17"/>
-        <location filename="../mainwindow.ui" line="57"/>
-        <location filename="../mainwindow.cpp" line="113"/>
+        <location filename="../../src/mainwindow.ui" line="17"/>
+        <location filename="../../src/mainwindow.ui" line="66"/>
+        <location filename="../../src/mainwindow.cpp" line="114"/>
         <source>Kutegram</source>
-        <translation></translation>
+        <translation>Kutegram</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="67"/>
-        <source>Crossplatform Telegram client, based on Qt.</source>
-        <translation type="unfinished"></translation>
+        <location filename="../../src/mainwindow.ui" line="129"/>
+        <source>Please, enter your phone number in international format.</source>
+        <translation>Будь ласка, введіть свій номер телефону в міжнародному форматі.</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="130"/>
-        <source>Please, choose your country code and enter your phone number.</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="160"/>
-        <source>Send code</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="187"/>
-        <source>Enter a confirmation code:</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="200"/>
-        <source>Confirm</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="207"/>
+        <location filename="../../src/mainwindow.ui" line="196"/>
         <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
 &lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
 p, li { white-space: pre-wrap; }
-&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;MS Shell Dlg 2&apos;; font-size:8.25pt; font-weight:400; font-style:normal;&quot;&gt;
-&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;a href=&quot;kutegram-change-phone&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Change phone number...&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+&lt;/style&gt;&lt;/head&gt;&lt;body&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;a href=&quot;kutegram://change-phone&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Change phone number...&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;a href=&quot;kutegram://change-phone&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt; Змінити номер телефону...&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="269"/>
+        <location filename="../../src/mainwindow.ui" line="258"/>
         <source>Actions</source>
         <translation>Дії</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="280"/>
+        <location filename="../../src/mainwindow.ui" line="269"/>
         <source>Help</source>
-        <translation>Допомога</translation>
+        <translation>Довідка</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="103"/>
-        <location filename="../mainwindow.ui" line="290"/>
+        <location filename="../../src/mainwindow.ui" line="102"/>
+        <location filename="../../src/mainwindow.ui" line="279"/>
         <source>Log in</source>
-        <translation>Авторизуватись</translation>
+        <translation>Увійти</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="56"/>
-        <source>Exit</source>
-        <translation>Вихід</translation>
+        <location filename="../../src/mainwindow.ui" line="76"/>
+        <source>Crossplatform Telegram client, based on Qt.</source>
+        <translation>Кросплатформний клієнт Telegram, заснований на Qt.</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="295"/>
+        <location filename="../../src/mainwindow.ui" line="149"/>
+        <source>Send code</source>
+        <translation>Надіслати код</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="176"/>
+        <source>Enter a confirmation code:</source>
+        <translation>Введіть код підтвердження:</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="189"/>
+        <source>Confirm</source>
+        <translation>Підтвердити</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="284"/>
         <source>Quit</source>
-        <translation type="unfinished">Вийти</translation>
+        <translation>Вийти</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="303"/>
+        <location filename="../../src/mainwindow.ui" line="292"/>
         <source>Log out</source>
-        <translation>Вийти з акауту</translation>
+        <translation>Вийти з аккаунту</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="308"/>
+        <location filename="../../src/mainwindow.ui" line="297"/>
         <source>About Kutegram</source>
         <translation>Про Kutegram</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="316"/>
+        <location filename="../../src/mainwindow.ui" line="305"/>
         <source>About Qt</source>
         <translation>Про Qt</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="31"/>
+        <location filename="../../src/mainwindow.cpp" line="31"/>
         <source>Phone number</source>
-        <translation type="unfinished">Номер телефону</translation>
+        <translation>Номер телефону</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="32"/>
+        <location filename="../../src/mainwindow.cpp" line="32"/>
         <source>Code</source>
-        <translation type="unfinished"></translation>
+        <translation>Код</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="41"/>
+        <location filename="../../src/mainwindow.cpp" line="41"/>
         <source>Options</source>
         <translation>Опції</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="114"/>
+        <location filename="../../src/mainwindow.cpp" line="56"/>
+        <source>Exit</source>
+        <translation>Вихід</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.cpp" line="115"/>
         <source>Kutegram by curoviyxru
 An unofficial Qt-based client for Telegram messenger.
 Project&apos;s website: http://kg.curoviyx.ru
@@ -157,44 +141,37 @@ Telegram channel: https://t.me/kutegram
 Telegram chat: https://t.me/kutegramchat</source>
         <translation>Kutegram, зробленний curoviyxru
 Неофіційний клієнт Telegram, оснований на Qt.
-Сайт проекту: http://kg.curoviyx.ru
-Канал Telegram: https://t.me/kutegram
-Чат Telegram: https://t.me/kutegramchat</translation>
+Веб-сайт проекту: http://kg.curoviyx.ru
+Telegram-канал: https://t.me/kutegram
+Чат в Telegram: https://t.me/kutegramchat</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="148"/>
-        <location filename="../mainwindow.cpp" line="158"/>
-        <location filename="../mainwindow.cpp" line="168"/>
-        <location filename="../mainwindow.cpp" line="184"/>
+        <location filename="../../src/mainwindow.cpp" line="150"/>
+        <location filename="../../src/mainwindow.cpp" line="160"/>
+        <location filename="../../src/mainwindow.cpp" line="170"/>
+        <location filename="../../src/mainwindow.cpp" line="186"/>
         <source>Error</source>
         <translation>Помилка</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="149"/>
+        <location filename="../../src/mainwindow.cpp" line="151"/>
         <source>Got socket error: %1</source>
-        <translation>Помилка сокету: %1</translation>
+        <translation>Помилка сокета: %1</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="159"/>
+        <location filename="../../src/mainwindow.cpp" line="161"/>
         <source>Got MT error: %1</source>
-        <translation>Помилка МТ: %1</translation>
+        <translation>Помилка MT: %1</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="169"/>
+        <location filename="../../src/mainwindow.cpp" line="171"/>
         <source>Got DH error: %1</source>
         <translation>Помилка DH: %1</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="185"/>
+        <location filename="../../src/mainwindow.cpp" line="187"/>
         <source>Got RPC error: %1, %2, %3</source>
         <translation>Помилка RPC: %1, %2, %3</translation>
-    </message>
-</context>
-<context>
-    <name>QRLoginDialog</name>
-    <message>
-        <source>QR-code login</source>
-        <translation type="obsolete">Авторизація за QR-кодом</translation>
     </message>
 </context>
 </TS>
